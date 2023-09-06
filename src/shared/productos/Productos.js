50a1a4ecea48d0d721247cc7648094d6d138cedd
lionezel <lionezelfts@gmail.com>
2023-09-06 16:40:26 -0500

    editar los productos

--- src/shared/productos/Productos.js
+++ src/shared/productos/Productos.js
@@ -1,42 +1,61 @@
-import React from "react";
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import React, { useContext, useRef } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { FirebaseContext } from "../../firebase";
 
 export const ProductoCard = ({ producto }) => {
-  const { nombre, imagen, existencia, categoria, precio, descripcion } = producto;
+  //Existencia ref para acceder al vbalor directamente
+  const existenciaRef = useRef(producto.existencia);
+
+  //Context de firebase para combios de la db
+  const { firebase } = useContext(FirebaseContext);
+
+  //Modificar el estado del produto en firebase
+  const actualizarDisponibilidad = () => {
+    const existencia = existenciaRef.current.value === "true";
+
+    try {
+      firebase.db.collection("productos").doc(id).update({ existencia });
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(existencia);
+  };
+
+  const { id, nombre, imagen, existencia, categoria, precio, descripcion } =
+    producto;
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image={imagen}
-        title="green iguana"
-      />
+      <CardMedia sx={{ height: 140 }} image={imagen} title="green iguana" />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {nombre}
         </Typography>
         <Typography gutterBottom variant="h9" component="div">
-           {categoria}
+          {categoria}
         </Typography>
-
 
         <Typography variant="body2" color="text.secondary">
           {descripcion}
         </Typography>
       </CardContent>
       <Typography gutterBottom variant="h9" component="div">
-           <label>
-              <span>existencia</span>
-              <select value={existencia}>
-                <option value="true">Disponible</option>
-                <option value="false">No disponible</option>
-              </select>
-           </label>
-        </Typography>
+        <label>
+          <span>existencia</span>
+          <select
+            value={existencia}
+            ref={existenciaRef}
+            onChange={() => actualizarDisponibilidad()}
+          >
+            <option value="true">Disponible</option>
+            <option value="false">No disponible</option>
+          </select>
+        </label>
+      </Typography>
     </Card>
   );
 };
