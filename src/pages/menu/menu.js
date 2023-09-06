89730b1a6cb165c4bfe5b6258de40ae49370ec5e
lionezel <lionezelfts@gmail.com>
2023-09-06 16:02:16 -0500

    snapshot de la base de datos

--- src/pages/menu/menu.js
+++ src/pages/menu/menu.js
@@ -1,29 +1,38 @@
-import React, { useContext, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { FirebaseContext } from '../../firebase'
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { FirebaseContext } from "../../firebase";
 
 export const Menu = () => {
+  //Definir el state para los productos
+  const [productos, setProductos] = useState([]);
 
   const { firebase } = useContext(FirebaseContext);
 
+  //Consultar la base de datos al cargar
   useEffect(() => {
     const obtenerProductos = () => {
-      const resultado = firebase.db.collection('productos').onSnapshot(manejarSnapshot);
-      console.log(resultado)
-    } 
-    obtenerProductos()
-  }, [])
+      firebase.db.collection("productos").onSnapshot(manejarSnapshot);
+    };
+    obtenerProductos();
+  }, []);
 
   //snapshot nos permite utilizar la base de datos en timepo real de firestore
-  function manejarSnapshot(snapshot){
+  function manejarSnapshot(snapshot) {
+    const productos = snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
 
+    //Almecenar los resultados en el state
+    setProductos(productos);
   }
-
 
   return (
     <>
-    <div>menu</div>
-    <Link to="/nuevo-platillo">Agregar platillo</Link>
+      <div>menu</div>
+      <Link to="/nuevo-platillo">Agregar platillo</Link>
     </>
-  )
-}
+  );
+};
