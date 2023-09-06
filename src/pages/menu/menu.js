0736202bee282a4b7664615efa2c3270eed8617d
lionezel <lionezelfts@gmail.com>
2023-09-06 15:53:11 -0500

    mostrando los datos

--- src/pages/menu/menu.js
+++ src/pages/menu/menu.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { FirebaseContext } from '../../firebase'
 
 export const Menu = () => {
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      const resultado = firebase.db.collection('productos').onSnapshot(manejarSnapshot);
+      console.log(resultado)
+    } 
+    obtenerProductos()
+  }, [])
+
+  //snapshot nos permite utilizar la base de datos en timepo real de firestore
+  function manejarSnapshot(snapshot){
+
+  }
+
+
   return (
     <>
     <div>menu</div>
