3ec59b9834920f3f49dfa92560682207c834f8dc
lionezel <lionezelfts@gmail.com>
2023-09-08 16:02:36 -0500

    mandando las ordenes a la pagina de ordenes

--- src/pages/ordenes/ordenes.js
+++ src/pages/ordenes/ordenes.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { FirebaseContext } from "../../firebase";
 
 export const Ordenes = () => {
-  return (
-    <div>ordenes</div>
-  )
-}
+  //Context con las operaciones de firebase
+  const { firebase } = useContext(FirebaseContext);
+
+  //State con las ordenes
+  const [ordenes, setOrdenes] = useState([]);
+
+  useEffect(() => {
+    const obtenerOrdenes = () => {
+      firebase.db
+        .collection("ordenes")
+        .where("completado", "==", false)
+        .onSnapshot(manejarSnaphot);
+    };
+    obtenerOrdenes();
+  }, []);
+
+  function manejarSnaphot(snapshot) {
+    const ordenes = snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+
+    setOrdenes(ordenes);
+  }
+
+  return <div>ordenes</div>;
+};
